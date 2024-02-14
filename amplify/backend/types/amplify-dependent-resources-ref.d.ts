cdd9ed71157345dd86224bf8c6e7ac7b90b55c75
duncanlaidlaw <slamdunc123@gmail.com>
2024-02-14 08:25:48 +0000

    add storage and update graphql schema

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s333e85f56": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
